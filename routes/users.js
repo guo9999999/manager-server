89c824b1d46ca683fe96ed235530c9edf6376d62
gjw <1638649205@qq.com>
2022-04-22 20:23:56 +0800

    4-21-菜单管理

--- routes/users.js
+++ routes/users.js
@@ -17,12 +17,13 @@ router.prefix('/users')
  * 登录模块
  */
 router.post('/login', async (ctx) => {
-  const { username, password } = ctx.request.body
+  const { userName, password } = ctx.request.body
+  console.log(ctx.request.body)
   // 查找数据  findOne（查找的条件）查找一条数据
   const res = await User.findOne({
-    username,
+    userName,
     password
-  }).select('password')
+  }).select('userId')
   const data = res._doc
   const token = jwt.sign(
     {
@@ -40,7 +41,7 @@ router.post('/login', async (ctx) => {
       ctx.body = util.fail('账号或者密码错误')
     }
   } catch (error) {
-    ctx.body = util.fail(error.msg)
+    ctx.body = util.fail('账号或者密码错误')
   }
 })
 
